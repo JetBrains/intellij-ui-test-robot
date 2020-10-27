11270e6c249009a91bf49861456ae5f5a9f09286
Eugene Nizienko <eugene.nizienko@jetbrains.com>
2020-10-27 14:24:37 +0300

    added Copyright text

--- robot-server-plugin/src/main/kotlin/com/intellij/remoterobot/utils/LruCache.kt
+++ robot-server-plugin/src/main/kotlin/com/intellij/remoterobot/utils/LruCache.kt
@@ -1,3 +1,5 @@
+// Copyright 2000-2020 JetBrains s.r.o. Use of this source code is governed by the Apache 2.0 license that can be found in the LICENSE file.
+
 package com.intellij.remoterobot.utils
 
 class LruCache<K, V>(private val maxEntries: Int = 1000) : LinkedHashMap<K, V>(maxEntries, 0.75f, true) {
--- robot-server-plugin/src/main/resources/static/xpathEditor.js
+++ robot-server-plugin/src/main/resources/static/xpathEditor.js
@@ -1,3 +1,5 @@
+// Copyright 2000-2020 JetBrains s.r.o. Use of this source code is governed by the Apache 2.0 license that can be found in the LICENSE file.
+
 class XpathEditor {
     static get _goodAttributes() {
         return ['class', 'text', 'name', 'myaction', 'accessiblename']

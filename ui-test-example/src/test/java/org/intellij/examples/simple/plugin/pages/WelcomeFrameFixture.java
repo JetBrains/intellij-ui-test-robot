42a018a9a104c32c1b90fed99d847a7f631bde79
Eugene Nizienko <eugene.nizienko@jetbrains.com>
2020-10-29 14:11:33 +0300

    ui-test-example fixes, respect very first run

--- ui-test-example/src/test/java/org/intellij/examples/simple/plugin/pages/WelcomeFrameFixture.java
+++ ui-test-example/src/test/java/org/intellij/examples/simple/plugin/pages/WelcomeFrameFixture.java
@@ -1,7 +1,6 @@
 // Copyright 2000-2020 JetBrains s.r.o. Use of this source code is governed by the Apache 2.0 license that can be found in the LICENSE file.
 
 package org.intellij.examples.simple.plugin.pages;
-
 import com.intellij.remoterobot.RemoteRobot;
 import com.intellij.remoterobot.data.RemoteComponent;
 import com.intellij.remoterobot.fixtures.ComponentFixture;
@@ -21,7 +20,7 @@ public class WelcomeFrameFixture extends ContainerFixture {
     }
 
     public ActionLinkFixture createNewProjectLink() {
-        return find(ActionLinkFixture.class, byXpath("//div[@accessiblename='New Project' and @class='MainButton']"));
+        return find(ActionLinkFixture.class, byXpath("//div[(@accessiblename='New Project' and @class='MainButton') or (@accessiblename='New Project' and @class='JButton')]"));
     }
 
     public ComponentFixture importProjectLink() {
--- ui-test-example/src/test/kotlin/org/intellij/examples/simple/plugin/pages/WelcomeFrame.kt
+++ ui-test-example/src/test/kotlin/org/intellij/examples/simple/plugin/pages/WelcomeFrame.kt
@@ -16,7 +16,7 @@ fun RemoteRobot.welcomeFrame(function: WelcomeFrame.()-> Unit) {
 @DefaultXpath("type", "//div[@class='FlatWelcomeFrame']")
 class WelcomeFrame(remoteRobot: RemoteRobot, remoteComponent: RemoteComponent) : CommonContainerFixture(remoteRobot, remoteComponent) {
     val createNewProjectLink
-        get() = actionLink(byXpath("New Project","//div[@class='MainButton' and @text='New Project']"))
+        get() = actionLink(byXpath("New Project","//div[(@class='MainButton' and @text='New Project') or (@accessiblename='New Project' and @class='JButton')]"))
     val moreActions
         get() = button(byXpath("More Action", "//div[@accessiblename='More Actions' and @class='ActionButton']"))
 

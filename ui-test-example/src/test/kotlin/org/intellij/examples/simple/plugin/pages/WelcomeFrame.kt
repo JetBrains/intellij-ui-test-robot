10cf5c4f04453143475d707158e833164276bc6c
Eugene Nizienko <eugene.nizienko@jetbrains.com>
2020-10-29 13:10:50 +0300

    ui-test-example fixes

--- ui-test-example/src/test/kotlin/org/intellij/examples/simple/plugin/pages/WelcomeFrame.kt
+++ ui-test-example/src/test/kotlin/org/intellij/examples/simple/plugin/pages/WelcomeFrame.kt
@@ -5,18 +5,18 @@ package org.intellij.examples.simple.plugin.pages
 import com.intellij.remoterobot.RemoteRobot
 import com.intellij.remoterobot.data.RemoteComponent
 import com.intellij.remoterobot.fixtures.*
-import com.intellij.remoterobot.fixtures.ActionLinkFixture
 import com.intellij.remoterobot.search.locators.byXpath
+import java.time.Duration
 
 fun RemoteRobot.welcomeFrame(function: WelcomeFrame.()-> Unit) {
-    find(WelcomeFrame::class.java).apply(function)
+    find(WelcomeFrame::class.java, Duration.ofSeconds(10)).apply(function)
 }
 
 @FixtureName("Welcome Frame")
 @DefaultXpath("type", "//div[@class='FlatWelcomeFrame']")
 class WelcomeFrame(remoteRobot: RemoteRobot, remoteComponent: RemoteComponent) : CommonContainerFixture(remoteRobot, remoteComponent) {
     val createNewProjectLink
-        get() = actionLink(byXpath("New Project","//div[@class='JButton' and @text='New Project']"))
+        get() = actionLink(byXpath("New Project","//div[@class='MainButton' and @text='New Project']"))
     val moreActions
         get() = button(byXpath("More Action", "//div[@accessiblename='More Actions' and @class='ActionButton']"))
 

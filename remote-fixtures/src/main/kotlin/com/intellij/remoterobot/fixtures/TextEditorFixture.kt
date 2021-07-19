65787cc926e02d3ea60eb2b04481c9e914fa471c
eugene.nizienko <eugene.nizienko@jetbrains.com>
2021-07-19 13:08:09 +0200

    added useful methods to work with editor

--- remote-fixtures/src/main/kotlin/com/intellij/remoterobot/fixtures/TextEditorFixture.kt
+++ remote-fixtures/src/main/kotlin/com/intellij/remoterobot/fixtures/TextEditorFixture.kt
@@ -4,6 +4,7 @@ import com.google.gson.Gson
 import com.intellij.remoterobot.RemoteRobot
 import com.intellij.remoterobot.data.RemoteComponent
 import com.intellij.remoterobot.search.locators.byXpath
+import org.assertj.swing.core.MouseButton
 import java.awt.Point
 
 class TextEditorFixture(remoteRobot: RemoteRobot, remoteComponent: RemoteComponent) :
@@ -17,6 +18,9 @@ class TextEditorFixture(remoteRobot: RemoteRobot, remoteComponent: RemoteCompone
 
     val editor: EditorFixture
         get() = find(EditorFixture.locator)
+
+    val statusButton
+        get() = find<ComponentFixture>(byXpath("//div[@class='StatusButton']"))
 }
 
 
@@ -38,15 +42,18 @@ class GutterIcon(private val gutter: GutterFixture, private val gutterIconInfo: 
     }
 
     fun rightClick() {
-        gutter.runJs("""
+        gutter.runJs(
+            """
            robot.click(component, new Point(${point.x}, ${point.y}), MouseButton.RIGHT_BUTTON, 1) 
-        """)
+        """
+        )
     }
 
     override fun toString(): String {
         return "$lineNumber: $point $description"
     }
 }
+
 data class GutterIconInfo(val description: String, val lineNumber: Int, val point: Point)
 class GutterFixture(remoteRobot: RemoteRobot, remoteComponent: RemoteComponent) :
     ComponentFixture(remoteRobot, remoteComponent) {
@@ -93,5 +100,58 @@ class EditorFixture(remoteRobot: RemoteRobot, remoteComponent: RemoteComponent) 
     ContainerFixture(remoteRobot, remoteComponent) {
     companion object {
         val locator = byXpath("//div[@class='EditorComponentImpl']")
+    }
+
+    init {
+        runJs(
+            """
+            local.put('editor', component.getEditor())
+            local.put('document', component.getEditor().getDocument())
+        """
+        )
+    }
+
+    val text: String
+        get() = callJs("local.get('document').getText()")
+
+    val selectedText: String
+        get() = callJs(
+            """
+            let selectedText = local.get('editor').getSelectionModel().getSelectedText()
+            if (!selectedText) { selectedText = "" }
+            selectedText
+        """
+        )
+
+    val caretOffset: Int
+        get() = callJs("local.get('editor').getCaretModel().getOffset()", true)
+
+    val fileName: String
+        get() = callJs("local.get('editor').getVirtualFile().getName()", true)
+
+    val filePath: String
+        get() = callJs("local.get('editor').getVirtualFile().getPath()", true)
+
+    fun clickOnOffset(offset: Int, mouseButton: MouseButton = MouseButton.LEFT_BUTTON, times: Int = 1) {
+        scrollToOffset(offset)
+        runJs(
+            """
+            const editor = local.get('editor')
+            const visualPosition = editor.offsetToVisualPosition(${offset})
+            const clickPoint = editor.visualPositionToXY(visualPosition)
+            robot.click(component, new Point(clickPoint.x, clickPoint.y + 5), MouseButton.${mouseButton}, ${times})
+        """, true
+        )
+    }
+
+    fun scrollToOffset(offset: Int) {
+        runJs(
+            """
+                const editor = local.get('editor')
+                local.put('scrollingInProgress', true)
+                editor.getScrollingModel().scrollTo(editor.offsetToLogicalPosition(${offset}), com.intellij.openapi.editor.ScrollType.CENTER)
+            """, true
+        )
+        Thread.sleep(500)
     }
 }

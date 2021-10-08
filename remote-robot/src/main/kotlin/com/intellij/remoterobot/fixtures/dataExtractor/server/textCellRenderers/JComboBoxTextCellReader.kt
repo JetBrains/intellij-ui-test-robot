a0509a49cbfa81c8ba3a6159afe2e2d71fc72250
eugene.nizienko <eugene.nizienko@jetbrains.com>
2021-10-08 09:58:38 +0200

    fixed compilation error

--- remote-robot/src/main/kotlin/com/intellij/remoterobot/fixtures/dataExtractor/server/textCellRenderers/JComboBoxTextCellReader.kt
+++ remote-robot/src/main/kotlin/com/intellij/remoterobot/fixtures/dataExtractor/server/textCellRenderers/JComboBoxTextCellReader.kt
@@ -3,13 +3,12 @@ package com.intellij.remoterobot.fixtures.dataExtractor.server.textCellRenderers
 import com.intellij.remoterobot.fixtures.dataExtractor.server.TextParser
 import org.assertj.swing.cell.JComboBoxCellReader
 import java.awt.Dimension
-import javax.annotation.Nonnull
 import javax.swing.JComboBox
 import javax.swing.JList
 import javax.swing.ListCellRenderer
 
 class JComboBoxTextCellReader : JComboBoxCellReader {
-    override fun valueAt(@Nonnull comboBox: JComboBox<*>, index: Int): String? {
+    override fun valueAt(comboBox: JComboBox<*>, index: Int): String? {
         return computeOnEdt {
             @Suppress("UNCHECKED_CAST") val renderer = comboBox.renderer as ListCellRenderer<Any>
             val c = renderer.getListCellRendererComponent(JList(), comboBox.getItemAt(index), index, true, true)

2418142ab492f2d5378c2e104ac8a1bd3c7e056f
eugene.nizienko <eugene.nizienko@jetbrains.com>
2021-10-26 11:06:37 +0200

    #111 added steps logging to the waitFor

--- remote-robot/src/main/kotlin/com/intellij/remoterobot/utils/RepeatUtils.kt
+++ remote-robot/src/main/kotlin/com/intellij/remoterobot/utils/RepeatUtils.kt
@@ -2,15 +2,17 @@
 
 package com.intellij.remoterobot.utils
 
+import com.intellij.remoterobot.stepsProcessing.step
 import java.time.Duration
 
 fun waitForIgnoringError(
     duration: Duration = Duration.ofSeconds(5),
     interval: Duration = Duration.ofSeconds(2),
+    description: String? = null,
     errorMessage: String = "",
     condition: () -> Boolean
 ) {
-    waitFor(duration, interval, errorMessage) {
+    waitFor(duration, interval, description, errorMessage) {
         try {
             condition()
         } catch (e: Throwable) {
@@ -23,38 +25,61 @@ fun waitForIgnoringError(
 fun waitFor(
     duration: Duration = Duration.ofSeconds(5),
     interval: Duration = Duration.ofSeconds(2),
+    description: String? = null,
     errorMessage: String = "",
     condition: () -> Boolean
 ) {
-    waitFor(duration, interval, { errorMessage }, condition)
+    waitFor(duration, interval, description, { errorMessage }, condition)
 }
 
 @JvmOverloads
 fun waitFor(
     duration: Duration = Duration.ofSeconds(5),
     interval: Duration = Duration.ofSeconds(2),
+    description: String? = null,
     errorMessageSupplier: () -> String,
     condition: () -> Boolean
 ) {
+    if (description != null) {
+        return step("Waiting for $description") {
+            if (repeatInTime(duration, interval, isLogEnabled = true) { condition() }) return@step
+            throw WaitForConditionTimeoutException(duration, errorMessageSupplier())
+        }
+    }
     if (repeatInTime(duration, interval) { condition() }) return
     throw WaitForConditionTimeoutException(duration, errorMessageSupplier())
 }
 
-class WaitForConditionTimeoutException(duration: Duration, errorMessage: String): IllegalStateException("Exceeded timeout ($duration) for condition function ${if (errorMessage.isNotEmpty()) "($errorMessage)" else ""} ")
+class WaitForConditionTimeoutException(duration: Duration, errorMessage: String) :
+    IllegalStateException("Exceeded timeout ($duration) for condition function ${if (errorMessage.isNotEmpty()) "($errorMessage)" else ""} ")
 
 fun <R> waitFor(
     duration: Duration,
     interval: Duration = Duration.ofSeconds(2),
+    description: String? = null,
     errorMessage: String = "",
     functionWithCondition: () -> Pair<Boolean, R>
 ): R {
     var result: R? = null
-    if (repeatInTime(duration, interval) {
-            val (condition, functionResult) = functionWithCondition()
-            result = functionResult
-            return@repeatInTime condition
-        }) {
-        return result!!
+    if (description != null) {
+        return step("Waiting for $description") {
+            if (repeatInTime(duration, interval, isLogEnabled = true) {
+                    val (condition, functionResult) = functionWithCondition()
+                    result = functionResult
+                    return@repeatInTime condition
+                }) {
+                return@step result!!
+            }
+            throw WaitForConditionTimeoutException(duration, errorMessage)
+        }
+    } else {
+        if (repeatInTime(duration, interval) {
+                val (condition, functionResult) = functionWithCondition()
+                result = functionResult
+                return@repeatInTime condition
+            }) {
+            return result!!
+        }
     }
     throw WaitForConditionTimeoutException(duration, errorMessage)
 }
@@ -62,15 +87,23 @@ fun <R> waitFor(
 fun repeatInTime(
     duration: Duration,
     interval: Duration = Duration.ofMillis(500),
+    isLogEnabled: Boolean = false,
     condition: () -> Boolean
 ): Boolean {
     val endTime = System.currentTimeMillis() + duration.toMillis()
-    while (System.currentTimeMillis() < endTime) {
-        if (condition())
+    var now = System.currentTimeMillis()
+    while (now < endTime) {
+        val isHappen = if (isLogEnabled) {
+            step("waiting(${endTime - now}ms left)") { return@step condition() }
+        } else {
+            condition()
+        }
+        if (isHappen)
             return true
         else {
             Thread.sleep(interval.toMillis())
         }
+        now = System.currentTimeMillis()
     }
     return false
 }

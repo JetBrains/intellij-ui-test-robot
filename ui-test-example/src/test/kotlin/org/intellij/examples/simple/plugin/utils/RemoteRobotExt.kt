8e0e3dfd9df8a26d8651a8f4d5c663be56e523a0
Vladislav Shishov <vladislav.shishov@jetbrains.com>
2021-12-29 16:53:12 +0300

    Revert "add ide-launcher test sample"
    This reverts commit 4f50a67c

--- ui-test-example/src/test/kotlin/org/intellij/examples/simple/plugin/utils/RemoteRobotExt.kt
+++
@@ -1,7 +0,0 @@
-package org.intellij.examples.simple.plugin.utils
-
-import com.intellij.remoterobot.RemoteRobot
-
-fun RemoteRobot.isAvailable(): Boolean = runCatching {
-    callJs<Boolean>("true")
-}.getOrDefault(false)

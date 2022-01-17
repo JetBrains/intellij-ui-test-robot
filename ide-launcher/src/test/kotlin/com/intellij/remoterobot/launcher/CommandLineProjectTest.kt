11d9fdae483f44a6c8fb26e97b12bf6e1f7b9168
Vladislav Shishov <vladislav.shishov@jetbrains.com>
2022-01-17 23:44:26 +0300

    fix CommandLineProjectTest

--- ide-launcher/src/test/kotlin/com/intellij/remoterobot/launcher/CommandLineProjectTest.kt
+++ ide-launcher/src/test/kotlin/com/intellij/remoterobot/launcher/CommandLineProjectTest.kt
@@ -4,6 +4,8 @@ import com.intellij.remoterobot.RemoteRobot
 import com.intellij.remoterobot.fixtures.CommonContainerFixture
 import com.intellij.remoterobot.fixtures.ComponentFixture
 import com.intellij.remoterobot.search.locators.byXpath
+import com.intellij.remoterobot.stepsProcessing.StepLogger
+import com.intellij.remoterobot.stepsProcessing.StepWorker
 import com.intellij.remoterobot.utils.Locators
 import com.intellij.remoterobot.utils.hasSingleComponent
 import com.intellij.remoterobot.utils.waitFor
@@ -11,6 +13,7 @@ import okhttp3.OkHttpClient
 import org.junit.jupiter.api.AfterAll
 import org.junit.jupiter.api.BeforeAll
 import org.junit.jupiter.api.Test
+import org.junit.jupiter.api.Timeout
 import org.junit.jupiter.api.extension.ExtendWith
 import org.junit.jupiter.api.extension.ExtensionContext
 import org.junit.jupiter.api.extension.TestWatcher
@@ -18,11 +21,13 @@ import java.io.File
 import java.nio.file.Files
 import java.nio.file.Path
 import java.time.Duration
+import java.util.concurrent.TimeUnit
 import javax.imageio.ImageIO
 import javax.swing.Box
 import javax.swing.JDialog
 
 @ExtendWith(CommandLineProjectTest.IdeTestWatcher::class)
+@Timeout(value = 25, unit = TimeUnit.MINUTES)
 class CommandLineProjectTest {
     companion object {
         private var ideaProcess: Process? = null
@@ -32,6 +37,8 @@ class CommandLineProjectTest {
         @BeforeAll
         @JvmStatic
         fun startIdea() {
+            StepWorker.registerProcessor(StepLogger())
+            
             val client = OkHttpClient()
             remoteRobot = RemoteRobot("http://localhost:8082", client)
             val ideDownloader = IdeDownloader(client)
@@ -71,13 +78,13 @@ class CommandLineProjectTest {
             button("Finish").click()
         }
         remoteRobot.find<CommonContainerFixture>(Locators.byProperties(Locators.XpathProperty.SIMPLE_CLASS_NAME to "IdeFrameImpl"), Duration.ofSeconds(20)).run {
-            find<CommonContainerFixture>(Locators.byProperties(Locators.XpathProperty.ACCESSIBLE_NAME to "Tip of the Day"), Duration.ofSeconds(20))
+            find<CommonContainerFixture>(Locators.byTypeAndProperties(JDialog::class.java, Locators.XpathProperty.ACCESSIBLE_NAME to "Tip of the Day"), Duration.ofSeconds(20))
                 .button("Close").click()
             runCatching { button("Got It").click() }
             find<CommonContainerFixture>(Locators.byType(Box::class.java))
                 .find<ComponentFixture>(Locators.byProperties(Locators.XpathProperty.TOOLTIP to "Run 'Main'"))
                 .click()
-            waitFor(Duration.ofSeconds(30)) {
+            waitFor(Duration.ofSeconds(60)) {
                 hasSingleComponent(Locators.byPropertiesContains(Locators.XpathProperty.TEXT to "Process finished with exit code 0"))
             }
         }
